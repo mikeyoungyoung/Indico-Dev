d0c24adbcf111958b3b65a9669a007e06952232c
Mike Young <mikeyoung.young@gmail.com>
2022-09-09 06:27:48 -0400

    add more comments and todos

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Overview of Indico Data and Databricks Integration
 
 - [ ] Validate the delta table operations work correctly
+- [ ] Add steps to mark submissions as retrieved when getting results
 - [ ] Build example of creating dataset from mounted buckets
 - [ ] Build example of retraining/staggered loop
 - [ ] Build example of how to extract predictions from Indico models, and merge with structured ML model and ML Flow
--- indico_spark_get_results.py
+++ indico_spark_get_results.py
@@ -13,10 +13,7 @@ import time
 import datetime
 import pandas as pd
 import logging
-import importlib
 import os
-import sys
-import requests
 import indico
 timestr = time.strftime("%Y%m%d-%H%M%S")
 timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
@@ -70,7 +67,7 @@ def add_current_date(df):
 ################################################################################################
 def run(argv=None):
 
-    # TODO: Update job to run with workflow ID arg
+    # TODO: Update job to run with workflow ID arg??
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--workflow_id',dest='workflow_id',help='Workflow ID')
@@ -159,8 +156,12 @@ query ListSubmissions(
     # make submissions table from pandas
     subs_pdf = pd.DataFrame.from_dict(submissions_response['submissions']['submissions'])
     print(subs_pdf.dtypes)
+    # Filter to only one workflow for the results, probably better to do this in the Indico request
+    # Initial goal was to grab all submissions into a single dataframe
+    # It only became a problem when I was trying to get the results as a column in a single df
     subs_pdf = subs_pdf[subs_pdf.workflowId.eq(701)]
     
+    # We can probably dynamically build this schema
     submissiomns_schema = StructType([ \
         StructField("id",IntegerType(),True), \
         StructField("datasetId",IntegerType(),True), \
@@ -175,25 +176,29 @@ query ListSubmissions(
         StructField("reviews", StringType(), True)
     ])
     submissions_df = spark.createDataFrame(subs_pdf, schema=submissiomns_schema)
+    
+    # Data hygiene prior to getting results
+    # I was trying to get results from deleted files that were erroring out
+
     submissions_df.printSchema()
     submissions_df.groupBy('deleted').count().show()
     submissions_df = submissions_df.filter(submissions_df.deleted == False)
     submissions_df.groupBy('deleted').count().show()
     submissions_df.show()
     submissions_df = submissions_df.filter(submissions_df.resultFile.isNotNull())
-    # submissions_df.filter(submissions_df.deleted == False).show(5)
-    # complete_submissions = submissions_df.filter(submissions_df.workflowId == 701).filter(submissions_df.deleted == False)
+    
+    # UDF to get results as a json string column 
     def get_results(path):
         result = client.call(RetrieveStorageObject(path))
-    #     return json.loads(result)
         return json.dumps(result)
     resultsUDF = udf(lambda z: get_results(z),StringType())
+
     complete_with_results = submissions_df\
         .withColumn("Results", resultsUDF(submissions_df.resultFile).alias("ResultsString"))\
-        .withColumn("Run_Date", current_date())
+        .withColumn("Run_Date", current_date()) # add current date for snapshot purposes if getting results more than once
     complete_with_results.printSchema()
     complete_with_results.show(5)
-    # submissions_df.write.parquet('./submissions_raw')
+    
     print("Attempt to write deltatable")
     try:
         make_delta_table(complete_with_results,"indico_results_bronze","id")
@@ -203,7 +208,7 @@ query ListSubmissions(
         print("------------------- Writing Parquet File -------------------")
         complete_with_results.write.mode('overwrite').parquet('./complete_results_raw')
 
-  
+
     spark.stop()
 
 if __name__ == '__main__':
